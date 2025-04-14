dedb911491f5f6cad1d17f8af50b8b4097abf4ce
luphin <sebaszegarra@gmail.com>
2025-04-13 21:11:32 -0400

    Update de css y estructura en la mayoria de los componente para que tenga relación al Figma inicial

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,43 +1,44 @@
 interface UserCredentials {
-    name: string;
-    email: string;
-    password: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export async function registerUser(credentials: UserCredentials): Promise<boolean> {
+  const { name, email, password } = credentials;
+
+  // Simular autenticación
+  console.log("Logging in with:", name, email, password);
+
+  if (email === "test@test.com" && password === "test") {
+    localStorage.setItem("authToken", "fake-jwt-token");
+    console.log("Login successful");
+    return true;
   }
-  
-  export async function registerUser(credentials: UserCredentials): Promise<boolean> {
-      const { name, email, password } = credentials;
-      
-      // Simular autenticación
-      console.log('Logging in with:', name, email, password);
-      
-      if (email === 'test@test.com' && password === 'test') {
-          localStorage.setItem('authToken', 'fake-jwt-token');
-          console.log('Login successful');
-          return true;
-      }
-      return false;
+  return false;
+}
+
+export function logout(): void {
+  localStorage.removeItem("authToken");
+  window.location.href = "/login";
+}
+
+export function isAuthenticated(): boolean {
+  return localStorage.getItem("authToken") !== null;
+}
+
+export function isValidEmail(email: string): boolean {
+  // Aquí debes revisar si el email es válido o si hay alguna lógica más compleja
+  if (email === "test@test.com") {
+    return true;
   }
-  
-  export function logout(): void {
-      localStorage.removeItem('authToken');
-      window.location.href = "/login";
+  return false;
+}
+
+export function isRegistered(email: string): boolean {
+  // Aquí debes añadir la lógica para verificar si el correo está registrado
+  if (!email) {
+    return false;
   }
-  
-  export function isAuthenticated(): boolean {
-      return localStorage.getItem('authToken') !== null;
-  }
-  
-  export function isValidEmail(email: string): boolean {
-      // Aquí debes revisar si el email es válido o si hay alguna lógica más compleja
-      if (email === 'test@test.com') {
-          return true;
-      }
-      return false;
-  }
-  
-  export function isRegistered(email: string): boolean {
-      // Aquí debes añadir la lógica para verificar si el correo está registrado
-      if (!email) { return false }
-      return true; // Solo para pruebas
-  }
-  
+  return true; // Solo para pruebas
+}
